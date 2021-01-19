3451570ea55ad35dab2fb2eccce7b56011b6d305
ZenCrazyCat <zencrazycat@gmail.com>
2021-01-19 23:18:19 +0200

    Add "monobank" and "vkurse" parsing lambdas; Add Logger.; Decrease lambdas memory size.

--- src/handlers/parsers.py
+++ src/handlers/parsers.py
@@ -1,13 +1,22 @@
 from datetime import datetime
 from decimal import Decimal
 
+from aws_lambda_powertools import Logger
 import requests
 
 from src.aws_resources.dynamodb import save_rate
 from src.handlers.utils import generate_id
 
 
-def parse_privat(event, context):
+logger = Logger(service="rates-aggregator", level="DEBUG")
+
+USD = "USD"
+EUR = "EUR"
+RUB = "RUB"
+
+
+@logger.inject_lambda_context(log_event=True)
+def parse_privatbank(event, _):
     url = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5"
     rates = requests.get(url).json()
     print(rates)
@@ -15,10 +24,10 @@ def parse_privat(event, context):
         if rate["base_ccy"] == "UAH":
             rate_kwargs = {
                 "rate_id": generate_id(),
-                "currency": rate.get("ccy"),
+                "currency":  ccy if (ccy := rate.get("ccy")) != 'RUR' else RUB,
                 "buy": Decimal(rate["buy"][:6]),
                 "sale": Decimal(rate["sale"][:6]),
-                "source": "privat",
+                "source": "privatbank",
                 "created": round(datetime.timestamp(datetime.now()))
             }
             save_rate(**rate_kwargs)
@@ -26,17 +35,62 @@ def parse_privat(event, context):
     return event
 
 
-def parse_mono(event, context):
-    url = "https://api.monobank.ua/bank/currency"
-    response = requests.get(url).json()
-    print(response)
+@logger.inject_lambda_context(log_event=True)
+def parse_monobank(event, _):
+    url = 'https://api.monobank.ua/bank/currency'
+    response = requests.get(url)
+    if not response.ok:
+        return
 
-    return event
+    r_json = response.json()
+    if r_json != {'errorDescription': 'Too many requests'}:
+        for rate in r_json:
+            if (rate['currencyCodeA'] in {840, 978, 643}) and (rate['currencyCodeB'] == 980):
+                if rate['currencyCodeA'] == 840:
+                    currency = USD
+                elif rate['currencyCodeA'] == 978:
+                    currency = EUR
+                elif rate['currencyCodeA'] == 643:
+                    currency = RUB
+
+                rate_kwargs = {
+                    "rate_id": generate_id(),
+                    "currency": currency,
+                    "buy": Decimal(str(round(rate['rateBuy'], 3))),
+                    "sale": Decimal(str(round(rate['rateSell'], 3))),
+                    "source": "monobank",
+                    "created": round(datetime.timestamp(datetime.now()))
+                }
+                save_rate(**rate_kwargs)
 
 
-def parse_vkurse(event, context):
+@logger.inject_lambda_context(log_event=True)
+def parse_vkurse(event, _):
     url = "http://vkurse.dp.ua/course.json"
-    response = requests.get(url).json()
-    print(response)
+    response = requests.get(url)
+    if not response.ok:
+        return
 
-    return event
+    r_json = response.json()
+    for curr in r_json:
+        if curr == 'Dollar':
+            currency = USD
+        elif curr == 'Euro':
+            currency = EUR
+        else:
+            currency = RUB
+
+        buy = r_json[curr]['buy'].replace(',', '.')
+        sale = r_json[curr]['sale'].replace(',', '.')
+
+        rate_kwargs = {
+            "rate_id": generate_id(),
+            'currency': currency,
+            'buy': Decimal(buy),
+            'sale': Decimal(sale),
+            'source': "vkurse",
+            "created": round(datetime.timestamp(datetime.now()))
+        }
+
+        save_rate(**rate_kwargs)
+
